cb97de5fbd7d0d980178060ba0ff3583f8c8a717
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-03-19 08:12:44 -0300

    style: :lipstick: adding new global styles

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -20,7 +20,21 @@ export const GlobalStyles = createGlobalStyle`
   }
 
   h1, h2, h3, h4 h5, h6 {
+    margin: ${({ theme }) => theme.spacings.large} 0;
     font-family: ${({ theme }) => theme.font.family.secondary};
   }
 
+  p {
+    margin: ${({ theme }) => theme.spacings.large} 0;
+  }
+
+  ul, ol {
+    list-style: none;
+    margin: ${({ theme }) => theme.spacings.large};
+    padding: ${({ theme }) => theme.spacings.large};
+  }
+
+  a {
+    color: ${({ theme }) => theme.colors.secondaryColor};
+  }
 `
