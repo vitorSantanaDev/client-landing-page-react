4df1bec53795ee5afbfc33f8d1fc350eb608ff76
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-03-19 10:49:43 -0300

    test: :white_check_mark: improving tests

--- src/components/GridText/GridText.test.tsx
+++ src/components/GridText/GridText.test.tsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { screen } from '@testing-library/react'
 import { renderTheme } from '../../styles/theme/render-theme'
 
 import GridText from '.'
@@ -7,6 +8,18 @@ import mock from './mock'
 describe('<GridText />', () => {
   it('should render the component GridText', () => {
     const { container } = renderTheme(<GridText {...mock} />)
+    const headings = screen.getAllByRole('heading', { name: /teste/i })
+
+    expect(headings).toHaveLength(3)
+    expect(container.firstChild).toMatchSnapshot()
+  })
+  it('should not render the headings when grid prop is empyt', () => {
+    const { container } = renderTheme(
+      <GridText title="My grid" description="test" grid={[]} />
+    )
+    const headings = screen.queryAllByRole('heading', { name: /teste/i })
+
+    expect(headings).toHaveLength(0)
     expect(container.firstChild).toMatchSnapshot()
   })
 })
