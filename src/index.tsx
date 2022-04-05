df0713d9923537f0c5ab7c51ca039664814537ae
vitorSantanaDev <vitorsantana.developer@gmail.com>
2022-04-04 23:34:48 -0300

    feat: :truck: creating routes file

--- src/index.tsx
+++ src/index.tsx
@@ -1,17 +1,17 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-
-import Home from './templates/Home'
-
 import { ThemeProvider } from 'styled-components'
+
 import { theme } from './styles/theme/theme'
 import { GlobalStyles } from './styles/global'
+
+import AppRoutes from './routes/routes'
 
 ReactDOM.render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
       <GlobalStyles />
-      <Home />
+      <AppRoutes />
     </ThemeProvider>
   </React.StrictMode>,
   document.getElementById('root')
